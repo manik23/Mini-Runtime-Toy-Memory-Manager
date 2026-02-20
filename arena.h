91c43da89afba906a40031d423604601d7047e3b
manik mahajan <manik.x.mahajan@gmail.com>
2026-02-20 11:08:42 +0530

    docs: Make comments in `arena.h` more concise and direct by removing redundant phrases.

--- arena.h
+++ arena.h
@@ -52,125 +52,101 @@ struct lists {
   struct FreeBlock *head;
 
   /* start_address
-    is the start of the free block chain
-    it is used to traverse the free block chain
-    it is also used to find the end of the free block chain
+    is the starting address of the free block chain
   */
   void *start_address;
 
   /* end_address
-    is the end of the free block chain
-    it is used to traverse the free block chain
-    it is also used to find the end of the free block chain
+    is the ending address of the free block chain
   */
   void *end_address;
 
   /* free_head
-  use data data of free block to store next free  block
-  Example:
+    use data of free block to store next free  block
+    Example:
     By putting the * at the very beginning, we are performing an assignment to
-  that memory address.
+    that memory address.
 
-  *(struct FreeBlock **)addr = value; means: "Go to addr, and write the 8-byte
-  address value into that spot." Visualizing the "Corruption" Here is why this
-  is safe and doesn't destroy the metadata:
+    *(struct FreeBlock **)addr = value; means: "Go to addr, and write the 8-byte
+    address value into that spot." Visualizing the "Corruption" Here is why this
+    is safe and doesn't destroy the metadata:
 
-  A Free Block in Memory:
+    A Free Block in Memory:
 
-  [   Header (Metadata)   ][       Payload (Data Area)       ]
-  [ next | prev | size... ][ byte 1 | byte 2 | ... | byte 8  ]
-  ^ found                  ^ found + 1 (This is where the user data lives)
+    [   Header (Metadata)   ][       Payload (Data Area)       ]
+    [ next | prev | size... ][ byte 1 | byte 2 | ... | byte 8  ]
+    ^ found                  ^ found + 1 (This is where the user data lives)
 
     After your "Ghost Link" magic:
 
-  [   Header (Metadata)   ][       Payload (Data Area)       ]
-  [ next | prev | size... ][       ADDRESS OF NEXT FREE      ]
-  ^ found                  ^ found + 1 (We used the user's bytes!)
+    [   Header (Metadata)   ][       Payload (Data Area)       ]
+    [ next | prev | size... ][       ADDRESS OF NEXT FREE      ]
+    ^ found                  ^ found + 1 (We used the user's bytes!)
 
-  Metadata (Header): Completely untouched! The next and prev pointers for the
-  physical chain are safe. Payload: Overwritten with our free_head link. Since
-  the block is free, the user doesn't care that we destroyed their data.
+    Metadata (Header): Completely untouched! The next and prev pointers for the
+    physical chain are safe. Payload: Overwritten with our free_head link. Since
+    the block is free, the user doesn't care that we destroyed their data.
 
-  free_head is used to store the recently freed free block chain in LIFO manner
+    free_head is used to store the recently freed free block chain in LIFO
+    manner
   */
   struct FreeBlock *free_head;
 };
 
 struct Arena {
   /* start_address
-    is the start of the arena
-    it is used to traverse the arena
-    it is also used to find the end of the arena
+    is the starting address of the arena
   */
   void *start_address;
 
   /* end_address
-    is the end of the arena
-    it is used to traverse the arena
-    it is also used to find the end of the arena
+    is the ending address of the arena
   */
   void *end_address;
 
   /* free_lists
     is the array of free lists
-    it is used to store the free blocks
-    it is also used to find the free blocks
   */
   struct lists free_lists[9];
 
   /* next_fresh_page
-    is the next fresh page
-    it is used to store the next fresh page
-    it is also used to find the next fresh page
+    is the next fresh page that can be used for allocation by lists
   */
   void *next_fresh_page;
 
   /* current_page
-    is the current page
-    it is used to store the current page
-    it is also used to find the current page
+    is the current page address available for allocation
   */
   int current_page;
 
   /* total_size
     is the total size of the arena
-    it is used to store the total size of the arena
-    it is also used to find the total size of the arena
   */
   size_t total_size;
 };
 
 /* rootArena
-    it is used to store the root arena
-    it is also used to find the root arena
+    is the root arena
 */
 extern struct Arena *rootArena;
 
 /* allocate_from_arena
     is the function to allocate memory from the arena
-    it is used to allocate memory from the arena
-    it is also used to find the memory from the arena
 */
 void *allocate_from_arena(size_t size);
 
 /* free_from_arena
     is the function to free memory from the arena
-    it is used to free memory from the arena
-    it is also used to find the memory from the arena
 */
 void free_from_arena(void *ptr);
 
 /* arena_free
     is the function to free the arena
-    it is used to free the arena
-    it is also used to find the arena
 */
 void arena_free(struct Arena *arena);
 
 /* display_arena
     is the function to display the arena
-    it is used to display the arena
-    it is also used to find the arena
 */
 void display_arena(struct Arena *arena);
 
